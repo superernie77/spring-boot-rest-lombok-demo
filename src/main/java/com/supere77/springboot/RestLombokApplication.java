81aa4992d48697bea9bdfbaf297d1038f2e0f6eb
superernie77 <superernie77@gmail.com>
2017-09-14 11:33:18 +0200

    Initial Spring-Boot App

---
+++ src/main/java/com/supere77/springboot/RestLombokApplication.java
@@ -0,0 +1,12 @@
+package com.supere77.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLombokApplication.class, args);
+	}
+}
---
+++ src/test/java/com/supere77/springboot/RestLombokApplicationTests.java
@@ -0,0 +1,16 @@
+package com.supere77.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestLombokApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
