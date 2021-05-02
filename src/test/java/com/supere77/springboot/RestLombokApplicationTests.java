b232cfbce0c33388a1a6183be6e2751b374cf432
superernie77 <superernie77@gmail.com>
2021-05-02 11:35:36 +0200

    Updated to JUnit5

--- src/test/java/com/supere77/springboot/RestLombokApplicationTests.java
+++ src/test/java/com/supere77/springboot/RestLombokApplicationTests.java
@@ -1,19 +1,17 @@
 package com.supere77.springboot;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestLombokApplicationTests {
 	
@@ -27,7 +25,7 @@ public class RestLombokApplicationTests {
 		
 		ResponseEntity<String > result = template.getForEntity("http://localhost:"+randomServerPort+"/messages", String.class);
 		
-		assertThat(result.getStatusCode(), equalTo(HttpStatus.OK) );
+			assertThat(result.getStatusCode(), equalTo(HttpStatus.OK) );
 		
 	}
 
